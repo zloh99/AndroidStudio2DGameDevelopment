c9cd73b5b7db86a33768f22a955f1c10d4869eb3
zloh99 <zenithstuff@gmail.com>
2023-02-20 17:19:27 +0800

    Game loop

---
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/Game.java
@@ -0,0 +1,81 @@
+package com.example.androidstudio2dgamedevelopment;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Game manages all objects in the game and is responsible for updating all states and rendering all
+ * objects to screen
+ */
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private GameLoop gameLoop;
+    private Context context;
+
+    public Game(Context context) {
+        super(context);
+
+        //Get surface holder and add callback
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
+    }
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("FPS: " + averageFPS, 100, 200, paint);
+    }
+
+    public void update() {
+        // Update game state
+    }
+}
--- app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.androidstudio2dgamedevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Main activity is the entry point to our application
+ */
+public class MainActivity extends Activity {
+
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Set window to fullscreen
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //Set content view to game, so that only objects in the game class can be rendered to screen
+        setContentView(new Game(this));
     }
 }
